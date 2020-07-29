5fa780a700c918f66bc8985b38f54c6ce8c621dc
andreas <andschar@gmail.com>
2020-07-29 13:51:11 +0200

    load_on_exit argument

--- R/clean_workspace.R
+++ R/clean_workspace.R
@@ -2,6 +2,7 @@
 #' 
 #' @param keep Objects to not be removed from the workspace. Provide as 
 #' character vector
+#' @param load_on_exit character; Source a file on exit. Default: NULL - none.
 #' @param envir Envrionment to be cleaned. Deafault: .GlobalEnv
 #'
 #' @author Andreas Scharmueller, \email{andschar@@protonmail.com}
@@ -11,11 +12,18 @@
 #' @examples 
 #' clean_workspace()
 #' 
-clean_workspace = function(keep = NULL, envir = .GlobalEnv) {
+clean_workspace = function(keep = NULL,
+                           load_on_exit = NULL,
+                           envir = .GlobalEnv) {
+  # clean
   l = setdiff(ls(envir = envir),
               c("clean_workspace", keep))
   rm(list = l,
      envir = envir)
   gc()
+  # load
+  if (!is.null(load_on_exit)) {
+    source(load_on_exit)
+  }
 }
 
