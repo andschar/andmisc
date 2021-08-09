00c4f9ed2a5f570e3df5557b5ce522d7f464cb9f
andreas <andschar@gmail.com>
2021-08-09 15:45:51 +0200

    added list partial matchin functions

--- R/list_functions.R
+++ R/list_functions.R
@@ -47,6 +47,61 @@
 #' 
 `%lchin%` = function(lhs, rhs) {
   sapply(lhs, function(lhs) {
-    any(chmatch(unlist(lhs), rhs))
+    any(data.table::chmatch(unlist(lhs), rhs))
+  })
+}
+
+#' Helper functions to apply grepl() to list column elements
+#' 
+#' @param lhs Character vector.
+#' @param rhs Character vector that should be looked up in x.
+#'
+#' @import data.table
+#'
+#' @author Andreas Scharmueller, \email{andschar@@protonmail.com}
+#'
+#' @export
+#'
+#' @examples
+#' require(data.table)
+#' dt = data.table(a = c('R1', 'R2', 'R3', 'R4', 'R5'),
+#'                 b = list(c('A', 'B'),
+#'                 c('A'),
+#'                 list('A', 'B', 'C'),
+#'                 list(list(c('B', 'C'))),
+#'                 'B'))
+#' dt[ a %llike% '[0-3]' ]
+#' 
+`%llike%` = function(lhs, rhs) {
+  sapply(lhs, function(lhs) {
+    any(grepl(paste0(rhs, collapse = '|'), unlist(lhs)))
+  })
+}
+
+#' Helper functions to apply grepl() to list column elements
+#' 
+#' @param lhs Character vector.
+#' @param rhs Character vector that should be looked up in x.
+#'
+#' @import data.table
+#'
+#' @author Andreas Scharmueller, \email{andschar@@protonmail.com}
+#'
+#' @export
+#'
+#' @examples
+#' require(data.table)
+#' dt = data.table(a = c('R1', 'R2', 'R3', 'R4', 'R5'),
+#'                 b = list(c('A', 'B'),
+#'                 c('A'),
+#'                 list('A', 'B', 'C'),
+#'                 list(list(c('B', 'C'))),
+#'                 'B'))
+#' dt[ a %lilike% '[4-9]' ]
+#' 
+`%lilike%` = function(lhs, rhs) {
+  sapply(lhs, function(lhs) {
+    any(grepl(paste0(rhs, collapse = '|'), unlist(lhs),
+              ignore.case = TRUE))
   })
 }
