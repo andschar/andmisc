2a07c532fc02c577b811f2ec55b198795a914a5a
andreas <andschar@gmail.com>
2021-08-16 12:53:56 +0200

    left right functions

---
+++ R/left_right.R
@@ -0,0 +1,38 @@
+#' Function to trim n leading characters from string.
+#' 
+#' @description Inspired by PosgreSQL
+#' \url{https://www.postgresql.org/docs/current/functions-string.html}.
+#' 
+#' @param string String of vector of strings.
+#' @param n Number of characters to be trimmed
+#'
+#' @author Andreas Scharmueller, \email{andschar@@protonmail.com}
+#'
+#' @export
+#' 
+#' @examples
+#' v = c('1234567', 'abcdefg')
+#' left(v, 2)
+#' 
+left = function(string, n) {
+  substr(string, 1+n, nchar(string))
+}
+#' Function to trim n trailing characters from string.
+#' 
+#' @description Inspired by PosgreSQL
+#' \url{https://www.postgresql.org/docs/current/functions-string.html}.
+#' 
+#' @param string String of vector of strings.
+#' @param n Number of characters to be trimmed
+#'
+#' @author Andreas Scharmueller, \email{andschar@@protonmail.com}
+#'
+#' @export
+#' 
+#' @examples
+#' v = c('1234567', 'abcdefg')
+#' right(v, 2)
+#' 
+right = function (string, n) {
+  substr(string, 1, nchar(string)-n)
+}
