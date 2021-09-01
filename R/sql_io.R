307c432478b193695fc8ec105ac14b18afac6c5a
andreas <andschar@gmail.com>
2021-09-01 16:51:08 +0200

    write_sf function

--- R/sql_io.R
+++ R/sql_io.R
@@ -1,4 +1,4 @@
-#' Connection function that handles argument or file inputs
+#' Connection function that handles argument or file inputs.
 #' 
 #' @author Andreas Scharmueller, \email{andschar@@protonmail.com}
 #' 
@@ -58,7 +58,17 @@ connection = function(cred_file,
   )
 }
 
-#' Function to read from a database
+#' Helper function to address table.
+#'
+#' @author Andreas Scharmueller, \email{andschar@@protonmail.com}
+#' 
+#' @noRd
+#' 
+schema_tbl = function(schema, tbl) {
+  paste0(c(schema, tbl), collapse = ".")
+}
+
+#' Function to read from a database.
 #'
 #' @param query A query string.
 #' @param cred_file An .R file containing the necessary credentials to connect
@@ -115,65 +125,7 @@ read_query = function(query = NULL,
   data.table::as.data.table(dat)
 }
 
-#' Function to read spatial data from a database
-#'
-#' @param query A query string.
-#' @param cred_file An .R file containing the necessary credentials to connect
-#' to a Postgres database. Can contain variables: user, host, port, password,
-#' dbname.
-#' @param host Optional host. Overwritten if \code{host} is defined in
-#' cred_file.
-#' @param port Optional port Overwritten if \code{port} is defined in
-#' cred_file.
-#' @param dbname Optional dbname Overwritten if \code{dbname} is defined
-#' in cred_file.
-#' @param user Optional user. Overwritten if \code{user} is defined in
-#' cred_file.
-#' @param password Optional password Overwritten if \code{password} is defined
-#' in cred_file.
-#' 
-#' @author Andreas Scharmueller, \email{andschar@@protonmail.com}
-#' 
-#' @export
-#' 
-#' @examples
-#' \donttest{
-#' # connection to database required
-#' read_spatial(query = "SELECT * FROM schema.table LIMIT 1;",
-#'              cred_file = cred.R)
-#' }
-#' 
-read_sf = function(query = NULL,
-                   cred_file = NULL,
-                   host = NULL,
-                   port = NULL,
-                   dbname = NULL,
-                   user = NULL,
-                   password = NULL) {
-  # checks
-  if (is.null(query)) {
-    stop('No query supplied.')
-  }
-  # connection
-  con = connection(cred_file = cred_file,
-                   host = host,
-                   port = port,
-                   dbname = dbname,
-                   user = user,
-                   password = password)
-  # bigint = 'integer') # to not return integer64 https://stackoverflow.com/questions/45171762/set-dbgetquery-to-return-integer64-as-integer
-  on.exit(DBI::dbDisconnect(con))
-  # query
-  message('Database: ', DBI::dbGetQuery(con, "SELECT current_database();"))
-  time = Sys.time()
-  dat = sf::st_read(con, query = query)
-  message('Query took: ', format(Sys.time() - time, digits = 1))
-  
-  dat
-}
-
-
-#' Function to send to a database
+#' Function to send to a database.
 #'
 #' @param query A query string.
 #' @param cred_file An .R file containing the necessary credentials to connect
@@ -228,7 +180,7 @@ send_query = function(query = NULL,
   message('Query took: ', format(Sys.time() - time, digits = 1))
 }
 
-#' Functions to write a table to a database
+#' Functions to write a table to a database.
 #'
 #' @param dat data.frame, data.table or tibble.
 #' @param schema database schema.
@@ -263,7 +215,7 @@ send_query = function(query = NULL,
 #' }
 #'
 write_tbl = function(dat = NULL,
-                     schema = 'public',
+                     schema = NULL,
                      tbl = NULL,
                      key = NULL,
                      comment_str = NULL,
@@ -292,7 +244,9 @@ write_tbl = function(dat = NULL,
   # query
   db = DBI::dbGetQuery(con, "SELECT current_database();")$current_database
   time = Sys.time()
-  DBI::dbSendQuery(con, paste0("CREATE SCHEMA IF NOT EXISTS ", schema, ";"))
+  if (!is.null(schema)) {
+    DBI::dbSendQuery(con, paste0("CREATE SCHEMA IF NOT EXISTS ", schema, ";"))
+  }
   DBI::dbWriteTable(con,
                     name = c(schema, tbl),
                     value = dat,
@@ -307,6 +261,151 @@ write_tbl = function(dat = NULL,
                                  "IS '", comment_str, "';"))
   }
   
+  message('Table created: ', paste0(c(db, schema, tbl), collapse = '.'))
+  message('Query took: ', format(Sys.time() - time, digits = 1))
+}
+
+#' Function to read spatial data from a database.
+#'
+#' @param query A query string.
+#' @param cred_file An .R file containing the necessary credentials to connect
+#' to a Postgres database. Can contain variables: user, host, port, password,
+#' dbname.
+#' @param host Optional host. Overwritten if \code{host} is defined in
+#' cred_file.
+#' @param port Optional port Overwritten if \code{port} is defined in
+#' cred_file.
+#' @param dbname Optional dbname Overwritten if \code{dbname} is defined
+#' in cred_file.
+#' @param user Optional user. Overwritten if \code{user} is defined in
+#' cred_file.
+#' @param password Optional password Overwritten if \code{password} is defined
+#' in cred_file.
+#' 
+#' @author Andreas Scharmueller, \email{andschar@@protonmail.com}
+#' 
+#' @export
+#' 
+#' @examples
+#' \donttest{
+#' # connection to database required
+#' read_spatial(query = "SELECT * FROM schema.table LIMIT 1;",
+#'              cred_file = cred.R)
+#' }
+#' 
+read_sf = function(query = NULL,
+                   cred_file = NULL,
+                   host = NULL,
+                   port = NULL,
+                   dbname = NULL,
+                   user = NULL,
+                   password = NULL) {
+  # checks
+  if (is.null(query)) {
+    stop('No query supplied.')
+  }
+  # connection
+  con = connection(cred_file = cred_file,
+                   host = host,
+                   port = port,
+                   dbname = dbname,
+                   user = user,
+                   password = password)
+  # bigint = 'integer') # to not return integer64 https://stackoverflow.com/questions/45171762/set-dbgetquery-to-return-integer64-as-integer
+  on.exit(DBI::dbDisconnect(con))
+  # query
+  message('Database: ', DBI::dbGetQuery(con, "SELECT current_database();"))
+  time = Sys.time()
+  message('Query took: ', format(Sys.time() - time, digits = 1))
+  sf::st_read(con, query = query)
+}
+
+#' Functions to write a spatial table to a database.
+#'
+#' @param dat a spatial data.frame (from the sf or sp package).
+#' @param schema database schema.
+#' @param tbl database table.
+#' @param overwrite Should an existing database table be overwritten?
+#' TRUE (default).
+#' @param key Set a primary key in table?
+#' @param comment_str Add a comment to table? Recommended.
+#' @param cred_file An .R file containing the necessary credentials to connect
+#' to a Postgres database. Can contain variables: user, host, port, password,
+#' dbname.
+#' @param user Optional user. Overwritten if \code{user} is defined in
+#' cred_file.
+#' @param host Optional host. Overwritten if \code{host} is defined in
+#' cred_file.
+#' @param port Optional port Overwritten if \code{port} is defined in
+#' cred_file.
+#' @param password Optional password Overwritten if \code{password} is defined
+#' in cred_file.
+#' @param dbname Optional dbname Overwritten if \code{dbname} is defined
+#' in cred_file.
+#' 
+#' @author Andreas Scharmueller, \email{andschar@@protonmail.com}
+#' 
+#' @export
+#' 
+#' @examples
+#' \donttest{
+#' # connection to database required
+#' dat = sf::st_sf(id = 1,
+#'                 sf::st_sfc(sf::st_point(1:2))) # better to name it!
+#' 
+#' write_sf(dat, tbl = 'test',
+#'          cred_file = 'TODO_cred.R',
+#'          overwrite = TRUE)
+#' }
+#'
+write_sf = function(dat = NULL,
+                    schema = NULL,
+                    tbl = NULL,
+                    key = NULL,
+                    comment_str = NULL,
+                    overwrite = TRUE,
+                    cred_file = NULL,
+                    host = NULL,
+                    port = NULL,
+                    dbname = NULL,
+                    user = NULL,
+                    password = NULL) {
+  # browser()
+  # checks
+  if (is.null(dat)) {
+    stop('No data.frame supplied.')
+  }
+  if (is.null(tbl)) {
+    stop('No table supplied.')
+  }
+  # connection
+  con = connection(cred_file = cred_file,
+                   host = host,
+                   port = port,
+                   dbname = dbname,
+                   user = user,
+                   password = password)
+  # bigint = 'integer') # to not return integer64 https://stackoverflow.com/questions/45171762/set-dbgetquery-to-return-integer64-as-integer
+  on.exit(DBI::dbDisconnect(con))
+  # query
+  db = DBI::dbGetQuery(con, "SELECT current_database();")$current_database
+  time = Sys.time()
+  if (!is.null(schema)) {
+    DBI::dbSendQuery(con, paste0("CREATE SCHEMA IF NOT EXISTS ", schema, ";"))
+  }
+  sf::st_write(dat,
+               dsn = con,
+               layer = c(schema, tbl),
+               delete_layer = overwrite,
+               row.names = FALSE)
+  if (!is.null(key)) {
+    DBI::dbSendQuery(con, paste0("ALTER TABLE ", schema_tbl(schema, tbl), " ",
+                                 "ADD PRIMARY KEY (", key, ");"))
+  }
+  if (!is.null(comment_str)) {
+    DBI::dbSendQuery(con, paste0("COMMENT ON TABLE ", schema_tbl(schema, tbl), " ",
+                                 "IS '", comment_str, "';"))
+  }
   message('Table created: ', paste0(c(db, schema, tbl), collapse = '.'))
   message('Query took: ', format(Sys.time() - time, digits = 1))
 }
