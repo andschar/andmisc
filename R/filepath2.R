6737878ba97c78b9c9b53bc065faed02f917e1a8
andreas <andschar@gmail.com>
2021-09-08 11:12:26 +0200

    file.path2()

---
+++ R/filepath2.R
@@ -0,0 +1,20 @@
+#' Function to Construct Path to Files
+#' 
+#' @description Works as \code{file.path()} but automatically creates
+#' a directory when missing.
+#'
+#' @param ... Character vectors as in \code{file.path()}.
+#' @param fsep Path seperator as in \code{file.path()}.
+#' 
+#' @author Andreas Scharmueller, \email{andschar@@protonmail.com}
+#' 
+#' @export
+#' 
+#' @examples 
+#' file.path2(tempdir(), 'test', 'blub')
+#' 
+file.path2 = function(..., fsep = .Platform$file.sep) {
+  path = file.path(..., fesp = fsep)
+  dir.create(path, showWarnings = FALSE)
+  path
+}
