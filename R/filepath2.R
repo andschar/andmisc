f3f31731adfb3d013a55c3e6af5f4b61ec81655c
andreas <andschar@gmail.com>
2021-09-20 15:34:54 +0200

    update

--- R/filepath2.R
+++ R/filepath2.R
@@ -4,7 +4,9 @@
 #' a directory when missing.
 #'
 #' @param ... Character vectors as in \code{file.path()}.
-#' @param fsep Path seperator as in \code{file.path()}.
+#' @param fsep Path separator as in \code{file.path()}.
+#' @param recursive As in \code{dir.create()}.
+#' @param mode As in \code{dir.create()}.
 #' 
 #' @author Andreas Scharmueller, \email{andschar@@protonmail.com}
 #' 
@@ -13,8 +15,15 @@
 #' @examples 
 #' file.path2(tempdir(), 'test', 'blub')
 #' 
-file.path2 = function(..., fsep = .Platform$file.sep) {
-  path = file.path(..., fsep = fsep)
-  dir.create(path, showWarnings = FALSE)
+file.path2 = function(...,
+                      recursive = TRUE,
+                      mode = '0777',
+                      fsep = .Platform$file.sep) {
+  path = file.path(...,
+                   fsep = fsep)
+  dir.create(path,
+             recursive = recursive,
+             mode = mode,
+             showWarnings = FALSE)
   path
 }
