0f9a593d262143116f21a0149f249f9708cc7167
andreas <andschar@gmail.com>
2021-10-20 13:41:53 +0200

    update fixweek()

--- R/fixweek.R
+++ R/fixweek.R
@@ -1,6 +1,7 @@
 #' Convenience function to turn week 1-9 into 01-09.
 #' 
-#' @description This is important in as.Date() parsing.
+#' @description Convenience function to turn week 1-9 into 01-09. Also rounds
+#' float values. This is important in as.Date() parsing.
 #'
 #' @param x a week number (as string or character).
 #' 
@@ -12,6 +13,6 @@
 #' fixweek(9)
 #' 
 fixweek = function(x) {
-  x = as.character(x)
+  x = as.character(round(x))
   data.table::fifelse(nchar(x) == 1, paste0('0', x), x)
 }
