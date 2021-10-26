b0f0b14fd91f696b386c6af3e6986a991aeabd97
andreas <andschar@gmail.com>
2021-10-26 15:05:38 +0200

    cut_labeler

---
+++ R/cut_labeler.R
@@ -0,0 +1,22 @@
+#' Function to produce nice Labels for the cut function
+#' 
+#' @param v A numeric vector of \code{cit()} breaks.
+#' 
+#' @author Andreas Scharmueller, \email{andschar@@protonmail.com}
+#' 
+#' @export
+#' 
+#' @examples 
+#' cut_labeler(c(5, 10, 15))
+#' 
+cut_labeler = function(x, round_digits = 0) {
+  v = c()
+  for (i in 1:(length(x)-1)) {
+    minimum = round(x[i], digits = round_digits)
+    maximum = round(x[i+1], digits = round_digits)
+    
+    v[i] = paste0(minimum, '-', maximum)
+  }
+  
+  v
+}
