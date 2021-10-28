beb72f852b8c5e6252d0b664d192771ef3813fe9
andreas <andschar@gmail.com>
2021-10-28 16:11:17 +0200

    vlaue separator function

---
+++ R/val_seperator.R
@@ -0,0 +1,40 @@
+#' Function to separate values at the nth position
+#' 
+#' @param v A numeric vector.
+#' @param n A numeric vector indicating the position to separate.
+#' 
+#' @author Andreas Scharmueller, \email{andschar@@protonmail.com}
+#' 
+#' @export
+#' 
+#' @examples 
+#' 
+#' v = 21032262 
+#' val_seperator(v)
+#' val_seperator(v, 2)
+#' 
+val_seperator = function(v, n = 3) {
+  # helper function
+  rev_str = function(x) {
+    unname(sapply(x, function(x) {
+        paste0(rev(strsplit(x, NULL)[[1]]), collapse = '')
+    }))
+  }
+  # chck
+  if (!all(is.numeric(v))) {
+    stop('Please provide a numeric value.')
+  }
+  v = as.character(v)
+  # reverse
+  v2 = rev_str(v)
+  # set seperator
+  v3 = sapply(v2, function(x) {
+    if (nchar(x) > n) {
+      gsub(paste0("(.{", n, "})"), "\\1,", x)
+    } else {
+      x
+    }
+  })
+  # reverse again
+  sub('^\\,', '', rev_str(v3))
+}
