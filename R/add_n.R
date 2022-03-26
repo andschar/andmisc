0caeb57eabc97731fa0525f122a03a08719b8d87
andreas <andschar@gmail.com>
2022-03-26 14:20:33 +0100

    add_n()

---
+++ R/add_n.R
@@ -0,0 +1,25 @@
+#' A function to the number of individual entries to a vector.
+#' 
+#' @description To be used with ggplot2::labeller().
+#' 
+#' @param l A vector.
+#' 
+#' @author Andreas Scharmueller, \email{andschar@@protonmail.com}
+#' 
+#' @export
+#'
+#' @examples 
+#' add_n(iris$Species)
+#'
+add_n = function(v) {
+  if (is.null(v)) {
+    stop('v needs to be a vector.')
+  }
+  vt = table(v)
+  vtn = names(vt)
+  vr = as.character(vt)
+  vr = paste0(vtn, '\n(n=', vr, ')')
+  names(vr) = vtn
+  
+  vr
+}
