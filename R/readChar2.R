ec210a13bac69b9fa65c770b7b3f233f23946a73
andreas <andschar@gmail.com>
2022-08-15 16:04:06 +0200

    added readChar2()

---
+++ R/readChar2.R
@@ -0,0 +1,17 @@
+#' Wrapper around \code{readChar()}
+#' 
+#' @param con A connection object, or a character string naming a file, or a raw vector.
+#' Same as in \code{readChar()}.
+#' 
+#' @author Andreas Scharmueller, \email{andschar@@protonmail.com}
+#' 
+#' @export
+#' 
+#' @examples
+#' tmp = tempfile()
+#' writeLines('Hello\nDarkness\nFriend', tmp)
+#' readChar2(tmp)
+#' 
+readChar2 = function(con) {
+  readChar(con, nchars = file.info(con)$size)
+}
