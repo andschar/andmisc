6cc5a0fdec0b5d149d5375c1d17798c42c103347
Andreas Scharmüller <andschar@proton.me>
2022-12-09 11:39:18 +0100

    new clean_workspace() strategy

--- R/clean_workspace.R
+++ R/clean_workspace.R
@@ -1,19 +1,20 @@
 #' Function to clean workspace appart from some variables
-#' 
-#' @param keep Objects to not be removed from the workspace. Provide as 
-#' character vector
-#' @param load_on_exit character; Source a file on exit. Default: NULL - none.
+#'
+#' @param keep Objects to not be removed from the workspace. Provide as
+#' character vector. By default getOption('keep_in_workspace') is called.
+#' @param load_on_exit Source a file on exit. Provide a file path. By default
+#' getOption('load_in_workspace') is called.
 #' @param envir Envrionment to be cleaned. Deafault: .GlobalEnv
 #'
 #' @author Andreas Scharmueller, \email{andschar@@protonmail.com}
-#' 
+#'
 #' @export
 #'
-#' @examples 
+#' @examples
 #' clean_workspace()
-#' 
-clean_workspace = function(keep = NULL,
-                           load_on_exit = NULL,
+#'
+clean_workspace = function(keep = getOption('keep_in_workspace'),
+                           load_on_exit = getOption('load_in_workspace'),
                            envir = .GlobalEnv) {
   # clean
   l = setdiff(ls(envir = envir),
@@ -26,4 +27,3 @@ clean_workspace = function(keep = NULL,
     source(load_on_exit)
   }
 }
-
